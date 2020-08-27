aee066854cca59dce4724bab04dbfdb9a51b3740
Morant1 <moran8865@walla.com>
2020-08-27 22:21:56 +0300

    home

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,9 +1,17 @@
-export default class Home extends React.Component {
+export class Home extends React.Component {
 
     render() {
         return (
-            <section>
-                <h2>Home Sweet Home</h2>
+            <section className="home-container">
+                <div className="position-img">
+                <div className="home-title">Welcome to AppSuS</div>
+                <div className="imgs">
+                    <img  className="img-home"src="assets\img\book.jpg"/>
+                    <img className="img-home" src="assets\img\gmail.png"/>
+                    <img className="img-home" src="assets\img\keep.jpg"/>
+                    </div>
+                    </div>
+                    <img className="main-img-container" src="assets\img\background.jpg"/>
             </section>
         )
     }
