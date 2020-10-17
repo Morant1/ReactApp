efb9a2786d52edcf22844d4237aec1cc036fe576
Moran Turgeman <61164854+Morant1@users.noreply.github.com>
2020-10-17 23:25:57 +0300

    Update google-books-service.js

--- apps/books/services/google-books-service.js
+++ apps/books/services/google-books-service.js
@@ -3,6 +3,6 @@ const axios = require("../../../lib/axios.js");
 
 export function getGoogleBooks(keyword) {
     // if (!keyword) return Promise.resolve()
-    return axios.get(`https://www.googleapis.com/books/v1/volumes?printType=books&q=${keyword}&maxResults=40`)
+    return axios.get(`https://www.googleapis.com/books/v1/volumes?printType=books&q=${keyword}&maxResults=40&key=AIzaSyDmya2Az9xeHL5r5HgyOL29s2hRTyUixLM`)
         .then(res => res.data)
 }
